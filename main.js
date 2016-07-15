281588378a8f7d7edf6665c226ec03b0027573e5
Liana Bravo <ldbravo04@gmail.com>
2016-07-15 13:01:44 -0400

    added score

--- main.js
+++ main.js
@@ -20,12 +20,17 @@ var mainState = {
 
         spaceKey.onDown.add(this.jump, this);
         this.pipes = game.add.group();
-        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0",
+       {font: "30px Arial", fill: "#ffffff" });
   },
 
     update: function() {
       if (this.bird.y < 0 || this.bird.y > 490){
          this.restartGame();
+         game.physics.arcade.overlap(
+             this.bird, this.pipes, this.restartGame, null, this);
       }
   },
   jump: function() {
@@ -68,6 +73,8 @@ addOnePipe: function(x, y) {
      for (var i = 0; i < 8; i++)
          if (i != hole && i != hole + 1)
              this.addOnePipe(400, i * 60 + 10);
+             this.score += 1;
+             this.labelScore.text = this.score;
  },
 };
 
